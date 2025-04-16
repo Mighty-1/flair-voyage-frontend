b0aaacfd3462bd68d5ee58fe7e08a14d4c60f5f2
MIGHTY AKUNEME <133228891+Mighty-1@users.noreply.github.com>
2025-04-16 18:17:51 +0100

    Delete BACKEND directory

--- BACKEND/routes/User.routes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const {newUser, loginUser} = require('../controllers/User.controller');
-
-const router = express.Router();
-
-router.post('/register', newUser);
-router.post('/login', loginUser);
-
-module.exports = router;
